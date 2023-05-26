710c14a209a018efa050010eb4886e37966f0c6f
MariaPilar10 <L202007042@vbravo.tecnm.mx>
2023-05-26 16:23:19 -0600

    Estilos app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<style>
+    div1{
+        height: 200px;
+        background-color: #EEE;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style> 
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="row">
+                    <div class="card" style="border:none; background-color: #f8fafc">
+                        <div class="card-body">
+                            <h2 class="card-title text-center">Riesgos en internet</h2>
+                            <p class="card-text text-center">
+                                <font size="3" color="#000000"> Bienvenido al sitio informativo sobre los malos usos de internet.
+                                    En este sitio encontrarás información detallada sobre las distintas formas en que internet puede ser mal utilizado y cómo protegerse de ellas.
+                                </font>
+                            </p>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                        <img src="/imagen/internet.png" class="card-img-bottom" style="wheight: 450px; height: 350px">
+                    </div> 
+                </div>         
             </div>
-        </div>
+        </div>  
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Click Seguro') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,26 +21,40 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         html {
-  min-height: 100%;
-  position: relative;
-}
+            min-height: 100%;
+            position: relative;
+        }
         footer {
-  background-color: grey;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 50px;
-  color: white;
+            background-color: #e7f0ed;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 50px;
+            color: white;
+            z-index: 1; /* Establecer el valor del z-index */
+            display: flex; /* Utilizar flexbox */
+            justify-content: flex-end; /* Alinear elementos al lado derecho */
+        }
+        .sidebar {
+            background-color: #f8f9fa;
+            border-right: 1px solid #dee2e6;
+            min-height: 100vh;
+            z-index: 2; /* Establecer el valor del z-index */
+        }
+        .content {
+            padding: 20px;
+            position: relative; /* Establecer la posición relativa */
+            z-index: 0; /* Establecer el valor del z-index */
         }
         
     </style> 
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light" style="background-color: #5d7da6;">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="/imagen/logo.jpg" style="width: 40px; height: 40px">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -113,12 +128,41 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1 sidebar" style="background-color: #7f9bc0">
+                    <!-- Contenido del sidebar -->
+                    <ul class="nav flex-column">
+                    <li class="nav-item">
+                         <a class="nav-link" href="#">Temas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Instituciones apoyo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Material de apoyo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Enciuestas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Resultados</a>
+                        </li>
+                        <!-- Agrega más elementos del sidebar si es necesario -->
+                    </ul>
+                </div>
+
+                <div class="col-md-9 content">
+                    <!-- Contenido principal -->
+                    <main class="py-4">
+                        @yield('content')
+                    </main>
+                </div>
+            </div>
+        </div>
     </div>
     <footer>
-    
+        <img src="/imagen/flecha.png" style="width: 40px; height: 40px" >
     </footer> 
 </body>
 </html>
